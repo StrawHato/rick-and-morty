8120e77d87cd9dff06f538fc3227ffa93ee435c2
Maksym Pakula <gargantadoto@gmail.com>
2026-03-17 04:16:45 +0200

    Add character list endpoint with filter

--- characters/urls.py
+++ characters/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from characters.views import get_random_character_view
+from characters.views import get_random_character_view, CharacterListView
 
 urlpatterns = [
     path("random", get_random_character_view, name="random-character"),
+    path("list/", CharacterListView.as_view(), name="character-list"),
 ]
 
 app_name = "characters"
