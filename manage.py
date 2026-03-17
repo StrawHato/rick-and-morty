b03e930027b13e414b97942f468ced62a18e1736
Maksym Pakula <gargantadoto@gmail.com>
2026-03-17 02:53:52 +0200

    Initialize characters app

--- manage.py
+++ manage.py
@@ -7,7 +7,9 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "rick_and_morty_API.settings")
+    os.environ.setdefault(
+        "DJANGO_SETTINGS_MODULE", "rick_and_morty_API.settings"
+    )
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
