d9d235689033097aff08f1a1ac7050108b1e7b98
Maksym Pakula <gargantadoto@gmail.com>
2026-03-18 15:49:15 +0200

    Add asyncio to Rick & Morty API to speed up scraping

--- characters/scraper.py
+++ characters/scraper.py
@@ -1,48 +1,86 @@
+import asyncio
 import time
 
-import requests
+import httpx
 from django.conf import settings
-from django.db import IntegrityError
+from httpx import AsyncClient
 
 from characters.models import Character
 
 
-def scrape_characters() -> list[Character]:
-    next_url_to_scrape = settings.RICK_AND_MORTY_API_CHARACTERS_URL
+GRAPHQL_QUERY = """
+query {
+  characters(page: %s) {
+    info{
+      pages
+    }
+    results{
+      api_id: id
+      name
+      status
+      gender
+      image
+    }
+  }
+}
+"""
 
-    characters = []
-    while next_url_to_scrape is not None:
-        characters_response = requests.get(next_url_to_scrape)
-        characters_response.raise_for_status()
 
-        data = characters_response.json()
+def parse_characters_response(characters_response: dict) -> list[Character]:
+    return [
+        Character(**character_dict)
+        for character_dict in characters_response["data"]["characters"]["results"]
+    ]
 
-        for character_dict in data["results"]:
-            characters.append(
-                Character(
-                    name=character_dict["name"],
-                    api_id=character_dict["id"],
-                    status=character_dict["status"],
-                    gender=character_dict["gender"],
-                    species=character_dict["species"],
-                    image=character_dict["image"],
-                )
+
+async def scrape_single_page(
+    client: AsyncClient, url_to_scrape: str, page: int
+) -> list[Character]:
+    characters_response = (
+        await client.post(url_to_scrape, data={"query": GRAPHQL_QUERY % str(page)})
+    ).json()
+    return parse_characters_response(characters_response)
+
+
+async def scrape_characters() -> list[Character]:
+    start = time.perf_counter()
+
+    url_to_scrape = settings.RICK_AND_MORTY_API_CHARACTERS_URL
+
+    characters_response = httpx.post(
+        url_to_scrape, data={"query": GRAPHQL_QUERY % "1"}
+    ).json()
+    num_pages = characters_response["data"]["characters"]["info"]["pages"]
+    characters = parse_characters_response(characters_response)
+
+    async with httpx.AsyncClient() as client:
+        characters.extend(
+            sum(
+                await asyncio.gather(
+                    *[
+                        scrape_single_page(client, url_to_scrape, page)
+                        for page in range(2, num_pages + 1)
+                    ]
+                ),
+                [],
             )
+        )
 
-        next_url_to_scrape = data["info"]["next"]
-        time.sleep(0.2)
+        end = time.perf_counter()
+        print("Elapsed for scraping:", end - start)
 
-    return characters
-
-
-def save_characters(characters: list[Character]):
-    for character in characters:
-        try:
-            character.save()
-        except IntegrityError:
-            print(f"Character with 'api_id'{character.api_id} already exists in database.")
+        return characters
 
 
-def sync_characters_with_api():
-    characters = scrape_characters()
-    save_characters(characters)
+async def save_characters(characters: list[Character]) -> None:
+    start = time.perf_counter()
+
+    await Character.objects.abulk_create(characters, ignore_conflicts=True)
+
+    end = time.perf_counter()
+    print("Elapsed for saving:", end - start)
+
+
+async def sync_characters_with_api() -> None:
+    characters = await scrape_characters()
+    await save_characters(characters)
